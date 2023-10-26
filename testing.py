2aac2a4627e2caf5c47b24c4688f3cf700cee3ca
kapil-Nauhwar <146325659+kapil-Nauhwar@users.noreply.github.com>
2023-10-26 12:01:17 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,16 @@
+# from polly.auth import Polly
+# from polly.omixatlas import OmixAtlas
+
+# # Create omixatlas object and authenticate
+# AUTH_TOKEN="a3kydXptc2JnMzo6VFBDRlpDbUk1TzhjV08wZXc0cmlvNjBwQmNIN1prek9wQ0kzZmZBZg=="
+# Polly.auth(AUTH_TOKEN, env="testpolly")
+# omixatlas = OmixAtlas()
+
+# repo_key = "geo" # repo_id or repo_name for the omixatlas
+# pp = omixatlas.omixatlas_summary(repo_key)
+# print(pp)
+
+ram = {"sri":"sri", "jay":"jay"}
+
+if "jay" in ram and "sri" in ram:
+    print("jay sri ram")
